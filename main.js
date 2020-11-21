590f077a86ea4d60be39d8a4c43366194a1711f3
bz068 <bz068@live.mdx.ac.uk>
2020-11-21 07:19:46 +0000

    project added

---
+++ main.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+ 
+fetch('http://api.weatherapi.com/v1/current.json?key=c3314bef8e5a42c792c72531202011&q=London').then(res => res.json()).then(data => {
+    document.getElementById('location').innerHTML = data.location.region;
+    document.getElementById('deg').innerHTML = data.current.temp_c + 'o'.sup();
+    document.getElementById('msg').innerHTML = data.current.condition.text;
+    
+
+
+});
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+
+
+// document.getElementById('location').innerHTML = 'res';
